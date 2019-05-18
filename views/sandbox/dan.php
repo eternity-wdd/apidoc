58c7e1a9d8cdd063f402bee67a6a50a2a73e39e1
LiZheng <{ID}+{username}@users.noreply.github.com>
2019-05-18 17:17:45 +0800

    1

--- views/sandbox/dan.php
+++ views/sandbox/dan.php
@@ -78,8 +78,9 @@ JQueryFormAsset::register($this, View::POS_READY);
     </div>
     <div>
     	<input type="hidden" name="m_id" value="<?php echo $_GET['id']?>"/>
+        <div id="env"><label>Host前缀：</label>  <input type="text" name="env" placeholder="比如: dev-" value="" />&nbsp;&nbsp;&nbsp;(非必填)</div>
         <div id="host"><label>Host：</label>  <input type="text" name="host" readonly="true" value=<?= $apis[0]['host']; ?> />&nbsp;&nbsp;&nbsp;(生产地址)</div>
-        <div id="env"><label>前缀：</label>  <input type="text" name="env" placeholder="比如: dev-" value="" />&nbsp;&nbsp;&nbsp;(非必填)</div>
+
 
         <label>接口：</label>
         <select id="api-selector" name="param[api]">
